d460df8394288283c13c64035208147659bdca84
Carl Smith <adkein@gmail.com>
2015-05-11 11:25:34 -0400

    add setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+setup(
+        name='textogram',
+        version='0.1',
+        description='Text-based histograms',
+        author='Carl Smith',
+        author_email='adkein@gmail.com',
+        url='https://github.com/adkein/textogram',
+        package_dir={'textogram': 'src'},
+        packages=['textogram'],
+        )
